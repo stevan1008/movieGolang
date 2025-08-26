4f57a48cda6108914b3fc56ff7a779b78544e8cb
Marlon Muete <stevan1008@hotmail.com>
2025-08-25 22:24:30 -0500

    feat: Add pkg model for rating service

---
+++ rating/pkg/rating.go
@@ -0,0 +1,27 @@
+package model
+
+// RecordID defines a record id. Together with RecordType
+// identifies unique records across all types.
+type RecordID string
+
+// RecordType defines a record type. Together with RecordID
+// identifies unique records across all types.
+type RecordType string
+
+// RecordTypeMovie Existing records types.
+const RecordTypeMovie = RecordType("movie")
+
+// UserID defines a user id.
+type UserID string
+
+// RatingValue defines a value of rating record.
+type RatingValue int
+
+// Rating defines an individual rating created by a user for
+// some record
+type Rating struct {
+	RecordID   string      `json:"recordId"`
+	RecordType string      `json:"recordType"`
+	UserID     UserID      `json:"userId"`
+	Value      RatingValue `json:"ratingValue"`
+}
