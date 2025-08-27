0d214b56f3949f0dfa21a7dcb367d1eab43008ec
Marlon Muete <stevan1008@hotmail.com>
2025-08-26 22:31:28 -0500

    feat: Adding Handler and Controller to PUT & GET methods for rating service

---
+++ rating/internal/handler/http/http.go
@@ -0,0 +1,64 @@
+package http
+
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/stevan1008/movieGolang/rating/internal/controller/rating"
+	model "github.com/stevan1008/movieGolang/rating/pkg"
+)
+
+// Handler defines a rating service controller.
+type Handler struct {
+	ctrl *rating.Controller
+}
+
+// New creates a new rating service HTTP handler.
+func New(ctrl *rating.Controller) *Handler {
+	return &Handler{ctrl}
+}
+
+// Handle handles PUT and GET /rating requests.
+func (h *Handler) Handle(w http.ResponseWriter, r *http.Request) {
+	recordID := model.RecordID(r.FormValue("id"))
+	if recordID == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	recordType := model.RecordType(r.FormValue("type"))
+	if recordType == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	switch r.Method {
+	case http.MethodGet:
+		aggregated, err := h.ctrl.GetAggregatedRating(r.Context(), recordID, recordType)
+		if err != nil && errors.Is(err, rating.ErrNotFound) {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		if err := json.NewEncoder(w).Encode(aggregated); err != nil {
+			log.Printf("Response encode error: %v/n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	case http.MethodPut:
+		userID := model.UserID(r.FormValue("userId"))
+		ratingValue, err := strconv.ParseFloat(r.FormValue("value"), 64)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if err := h.ctrl.PutRating(r.Context(), recordID, recordType, &model.Rating{UserID: userID, Value: model.RatingValue(ratingValue)}); err != nil {
+			log.Printf("Repository put error %v/n", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+}
